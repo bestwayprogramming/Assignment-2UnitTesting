ee88a10b00f72447c7e143d1b1633f1c9dc2dcd2
Vivek Dhavalbhai Patel <Vpatel05262@conestogac.on.ca>
2024-10-11 17:14:56 -0400

    Newly added

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Conestoga College")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © Conestoga College 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("459a4e33-1694-440a-b9e5-ef56a2dc3ccb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/TriangleTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+
+[TestClass]
+public class TriangleTests
+{
+    [TestMethod]
+    public void AnalyzeTriangle_Equilateral_ReturnsEquilateral()
+    { 
+        int side1 = 3, side2 = 3, side3 = 3;
+
+        string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+        Assert.AreEqual("Equilateral triangle", result);
+    }
+}
